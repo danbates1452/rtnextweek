edbcd47927588428504ce0879599267e4c77deeb
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2025-02-01 21:42:15 +0000

    add motion blur and bvh w/aabb

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include "rtweekend.h"
 
+#include "bvh.h"
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
@@ -26,7 +27,8 @@ int main() {
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
@@ -56,6 +58,7 @@ int main() {
     world.add(make_shared<sphere>(point3(4, 1, 3), 1.0, material1));
     world.add(make_shared<sphere>(point3(4, 1, 3), 0.8, material_bubble));
 
+    world = hittable_list(make_shared<bvh_node>(world));
     /*
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
@@ -86,9 +89,9 @@ int main() {
     */
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1200;
+    cam.image_width = 400;
     //crank samples to 500 for a nice crisp slow render, or leave at 10 for good iteration time
-    cam.samples_per_pixel = 10;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
