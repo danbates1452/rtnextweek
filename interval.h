701d2fe4523aa00e7074f2b868b72ebb3a7e8bac
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2025-03-06 21:15:13 +0000

    Pull in sample code as there's a breaking bug in mine somewhere to do with quads

--- interval.h
+++ interval.h
@@ -1,54 +1,65 @@
 #ifndef INTERVAL_H
 #define INTERVAL_H
+//==============================================================================================
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
+
 
 class interval {
-    public:
-        double min, max;
-        interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+  public:
+    double min, max;
 
-        interval(double min, double max) : min(min), max(max) {}
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
 
-        interval(const interval& a, const interval& b) {
-            // Create the interval tightly enclosing the two input intervals
-            min = a.min <= b.min ? a.min : b.min;
-            max = a.max >= b.max ? a.max : b.max;
-        }
+    interval(double min, double max) : min(min), max(max) {}
 
-        double size() const {
-            return max - min;
-        }
+    interval(const interval& a, const interval& b) {
+        // Create the interval tightly enclosing the two input intervals.
+        min = a.min <= b.min ? a.min : b.min;
+        max = a.max >= b.max ? a.max : b.max;
+    }
 
-        bool contains(double x) const {
-            return min <= x && x <= max;
-        }
+    double size() const {
+        return max - min;
+    }
 
-        bool surrounds(double x) const {
-            return min < x && x < max;
-        }
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
 
-        double clamp(double x) const {
-            if (x < min) return min;
-            if (x > max) return max;
-            return x;
-        }
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
 
-        interval expand(double delta) const {
-            auto padding = delta/2;
-            return interval(min - padding, max + padding);
-        }
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
 
-        static const interval empty, universe;
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
+    }
+
+    static const interval empty, universe;
 };
 
-const interval interval::empty = interval(+infinity, -infinity);
+const interval interval::empty    = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
 
 interval operator+(const interval& ival, double displacement) {
     return interval(ival.min + displacement, ival.max + displacement);
 }
 
-interval operator+(const double displacement, const interval& ival) {
+interval operator+(double displacement, const interval& ival) {
     return ival + displacement;
 }
+
 
 #endif
--- rtweekend.h
+++ rtweekend.h
@@ -1,5 +1,13 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
+//==============================================================================================
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
 
 #include <cmath>
 #include <cstdlib>
@@ -7,27 +15,32 @@
 #include <limits>
 #include <memory>
 
-// c++ std usings
+
+// C++ Std Usings
+
 using std::make_shared;
 using std::shared_ptr;
 
-//constants
+
+// Constants
+
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-//utility functions
+
+// Utility Functions
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
 inline double random_double() {
-    // Returns a random real in [0,1].
+    // Returns a random real in [0,1).
     return std::rand() / (RAND_MAX + 1.0);
 }
 
 inline double random_double(double min, double max) {
-    // Returns a random real in [min, max].
+    // Returns a random real in [min,max).
     return min + (max-min)*random_double();
 }
 
@@ -36,11 +49,13 @@ inline int random_int(int min, int max) {
     return int(random_double(min, max+1));
 }
 
-// common headers
+
+// Common Headers
 
 #include "color.h"
 #include "interval.h"
 #include "ray.h"
 #include "vec3.h"
+
 
 #endif
