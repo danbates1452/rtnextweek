65769a34bc56c758e25154496fad9b02df48b957
danbates1452 <danbates1452@gmail.com>
2025-12-16 19:50:23 +0000

    Set default scene to the fast version of the final scene

--- main.cc
+++ main.cc
@@ -395,7 +395,7 @@ void final_scene(int image_width, int samples_per_pixel, int max_depth) {
 
 
 int main() {
-    switch (9) {
+    switch (-1) {
         case 1: bouncing_spheres(); break;
         case 2: checkered_spheres(); break;
         case 3: earth(); break;
